5c71c204dc360c5136b9b360e6cd7e2615c63608
jharris4 <harris.jb@gmail.com>
2016-09-23 00:49:16 -0400

    Update examples...

--- example/example.js
+++ example/example.js
@@ -3,39 +3,39 @@
 
 require.ensure(['./a'], function() {
 	var a = require('./a');
-    console.log('success', a);
+    console.log('success require ensure 1', a);
 }, function() {
-    console.log('error');
+    console.log('error require ensure 1');
 }, '1');
 
 require.ensure(['./a'], function() {
 	var a = require('./a');
-    console.log('success', a);
+    console.log('success require ensure 2', a);
 }, function() {
-    console.log('error');
+    console.log('error require ensure 2');
 });
 
 require.ensure(['./a'], function() {
 	var a = require('./a');
-    console.log('success', a);
+    console.log('success require ensure 3', a);
 }, 'c');
 
 require.ensure(['./a'], function() {
 	var a = require('./a');
-    console.log('success', a);
+    console.log('success require ensure 4', a);
 });
 
 // AMD
 // require('./b'); // <-- prevent b chunking
 
 require(['./b'], function(b) {
-	console.log('success', b);
+	console.log('success AMD 1', b);
 }, function() {
-	console.log('error');
+	console.log('error AMD 1');
 });
 
 require(['./b'], function(b) {
-	console.log('success', b);
+	console.log('success AMD 2', b);
 });
 
 require(['./b']);
@@ -43,14 +43,14 @@ require(['./b']);
 // bundle loader
 var bundle = require('require-error-handler-webpack-plugin/src/BundleLoader!./a');
 bundle(function(a) {
-	console.log('success', a);
+	console.log('success bundle loader 1', a);
 }, function() {
-	console.log('error');
+	console.log('error bundle loader 1');
 });
 
 bundle = require('require-error-handler-webpack-plugin/src/BundleLoader?lazy!./a');
 bundle(function(a) {
-    console.log('success', a);
+    console.log('success bundle loader 2', a);
 }, function() {
-    console.log('error');
+    console.log('error bundle loader 2');
 });
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,7 +1,6 @@
 // webpack --config example/webpack.config.js
 
 var requireErrorHandler = require('require-error-handler-webpack-plugin');
-var JsonpMainTemplatePlugin = require('webpack/lib/JsonpMainTemplatePlugin');
 
 module.exports = {
     context: __dirname,
@@ -17,7 +16,6 @@ module.exports = {
     //     }
     // }
     plugins: [
-        new requireErrorHandler.JsonpErrorHandlerPlugin(JsonpMainTemplatePlugin),
         new requireErrorHandler.RequireEnsureErrorHandlerPlugin(),
         new requireErrorHandler.AMDRequireErrorHandlerPlugin()
     ]
